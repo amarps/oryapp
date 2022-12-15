88572c6448727cab0a7d0d2a03ee1ed966ed7cce
amarps <amarpanjis@gmail.com>
2022-12-16 02:47:18 +0700

    from client

---
+++ pages/register.tsx
@@ -0,0 +1,52 @@
+import { useRouter } from "next/router";
+import React, { SyntheticEvent, useState } from "react";
+import Layout from "../layouts/Layout";
+
+const Register = () => {
+    const [name,setName]=useState('');
+    const [email,setEmail]=useState('');
+    const [password,setPassword]=useState('');
+    const router = useRouter();
+    const submit = async (e:SyntheticEvent) => {
+        e.preventDefault();
+        await fetch('http://localhost:8000/api/register',{
+            method:'POST',
+            headers:{'Content-Type':'application/json'},
+            body:JSON.stringify({
+                name,
+                email,
+                password
+            })
+        });
+        await router.push('/login');
+    }
+
+    return (
+        <Layout>
+            <form onSubmit={submit}>
+    
+                <h1 className="h3 mb-3 fw-normal">Create an account</h1>
+
+
+                <input  className="form-control"  placeholder="Username" required
+                    onChange={e =>setName(e.target.value)}
+                />
+
+                <input type="email" className="form-control"  placeholder="name@example.com" required
+                onChange={e =>setEmail(e.target.value)}
+                />
+
+                <input type="password" className="form-control"  placeholder="Password" required
+                onChange={e =>setPassword(e.target.value)}
+                />
+
+
+
+                <button className="w-100 btn btn-lg btn-primary" type="submit">Submit</button>
+
+            </form>
+
+        </Layout>
+    );
+};
+export default Register;
